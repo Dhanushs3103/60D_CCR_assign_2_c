00a2f27ea99f9ec3379f6b39c82b325951d35c64
Dhanushs3103 <dhanushvictor2003@gmail.com>
2024-05-22 17:54:30 +0530

    completed the code of fetching data form API and displaying it on UI and implementing pagination

---
+++ problem_3/src/components/Post.jsx
@@ -0,0 +1,11 @@
+function Post({ title, body, id }) {
+  return (
+    <div>
+      <p>Title : {title}</p>
+      <p>Body : {body}</p>
+      <p>id : {id}</p>
+    </div>
+  );
+}
+
+export default Post;
